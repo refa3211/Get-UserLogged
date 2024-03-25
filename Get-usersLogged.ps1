e7e39a37f7cf48a324270572aa2ad195969fc5d2
refa3211 <111779078+refa3211@users.noreply.github.com>
2024-03-25 11:48:27 +0200

    Update and rename Get-user to Get-usersLogged.ps1

--- Get-usersLogged.ps1
+++ Get-usersLogged.ps1
@@ -6,12 +6,12 @@ $pclist = Get-ADComputer -Filter {Enabled -eq 'True'} -SearchBase $OU
 
 foreach ($pc in $pclist) {
     $pc = $pc.Name
-    if (Test-Connection -ComputerName $pc -Count 1 -ErrorAction "Ignore") {
+    if (Test-Connection -ComputerName $pc -Count 1 -ErrorAction "Quiet") {
         Write-Host $pc
         try {
             $last_user = (quser /server:$pc | Select-Object -Skip 1) -replace ' {1,}', ',' 
             $last_user_string = $last_user.ForEach({ "$pc,$_" }) -join "`n" | Out-File .\csv-1.csv -Append  -Encoding 'utf8' 
-            Write-Host "$pc, $last_user_string"
+            Write-Host "$pc,$last_user_string"
         } 
         catch {
             Write-Warning "Failed to retrieve user information from $pc"
